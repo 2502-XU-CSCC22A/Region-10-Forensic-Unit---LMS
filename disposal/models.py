6206890243cabbd1265844025a931c8df7fa351b
Precious Pagute <20210021374@my.xu.edu.ph>
2026-04-17 21:15:21 +0800

    Disposal App Updated: Added filter modal and table connection to database

--- disposal/models.py
+++ disposal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DisposalItem(models.Model):
+    unit = models.CharField(max_length=100)
+    sub_unit = models.CharField(max_length=100)
+    comm_type = models.CharField(max_length=100) # You can store "Fantasy, Fiction" here
+    expiry_status = models.DateField()
+    station_url = models.URLField()
+
+    def __str__(self):
+        return self.sub_unit
--- disposal/views.py
+++ disposal/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import DisposalItem
 
 # Create your views here.
 def disposal_list(request):
     return render(request, 'disposal/disposal.html')
+
+def disposal_view(request):
+    # This line queries your Supabase Postgres database
+    all_items = DisposalItem.objects.all() 
+    
+    return render(request, 'disposal/disposal.html', {'items': all_items})
