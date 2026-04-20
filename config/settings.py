cc62d4d662113c32d32667d9efd191b73f4c1287
PreciousPagute <20210021374@my.xu.edu.ph>
2026-04-20 21:40:24 +0800

    Config/settings.py changes

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions', 
     'disposal',
+    'config',
     'login',
     'django.contrib.messages',
     'django.contrib.staticfiles',
