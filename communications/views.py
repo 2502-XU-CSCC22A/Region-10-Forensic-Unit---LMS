d4b5b39312a4818af63c807ca2fac02bbf610979
Precious Pagute <20210021374@my.xu.edu.ph>
2026-05-05 14:05:38 +0800

    Send help ben fix activity_logs.html UI

--- communications/views.py
+++ communications/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Communication, CommunicationPARRecord
+from django.contrib.auth.models import User
 from .forms import CommunicationPARForm
 from django.utils import timezone
 import uuid
@@ -19,19 +20,55 @@ def communications_list(request):
     all_v = Vehicle.objects.all()
     all_d = DisposalItem.objects.all()
     
+    users = User.objects.select_related('userprofile') \
+        .filter(is_active=True) \
+        .order_by('-last_login')[:50]
+        
+    try:
+        current_user_role = request.user.userprofile.role
+        print("USER DEBUG:", repr(request.user.username))
+        print("ROLE DEBUG:", repr(current_user_role))
+    except Exception as e:
+        print("ROLE ERROR:", e)
+        current_user_role = None
+    
     return render(request, 'communications/communications.html', {
         'items': items,
         'total_comms': all_c.count() ,
         'total_ber': all_d.count(),
         'total_vehicles': all_v.count(),
-        'total_firearms': all_f.count()
+        'total_firearms': all_f.count(),
+        'current_user_role':  current_user_role
         })
 
 def par_monitoring(request):
     return render(request, 'communications/par_monitoring.html')
 
 def activity_logs(request):
-    return render(request, 'communications/activity_logs.html')
+    all_c = Communication.objects.exclude(status_id__in=[4, 5])
+    all_f = Firearm.objects.all()
+    all_v = Vehicle.objects.all()
+    all_d = DisposalItem.objects.all()
+    
+    users = User.objects.select_related('userprofile') \
+        .filter(is_active=True) \
+        .order_by('-last_login')[:50]
+        
+    try:
+        current_user_role = request.user.userprofile.role
+        print("USER DEBUG:", repr(request.user.username))
+        print("ROLE DEBUG:", repr(current_user_role))
+    except Exception as e:
+        print("ROLE ERROR:", e)
+        current_user_role = None
+    
+    return render(request, 'communications/activity_logs.html', {
+        'total_comms': all_c.count() ,
+        'total_ber': all_d.count(),
+        'total_vehicles': all_v.count(),
+        'total_firearms': all_f.count(),
+        'current_user_role':  current_user_role
+    })
 
 def par_monitoring(request):
     pars = CommunicationPARRecord.objects.select_related("communication").all().order_by("-created_at")
